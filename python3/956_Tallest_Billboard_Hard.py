0329e69399b1663e9adf23e8e0b00444ae766322
Wooyoung Choi <wychoi502@gmail.com>
2023-06-25 03:39:49 +0900

    956. Tallest Billboard - Hard

---
+++ python3/956_Tallest_Billboard_Hard.py
@@ -0,0 +1,28 @@
+# 956. Tallest Billboard - Hard
+
+956_Tallest_Billboard_Hard.py
+
+class Solution:
+    # return most highest height of mine that has differenet of diff to other's.
+    def helper(self, rods: List[int], index, diff, memo) -> int:
+        if (index, diff) in memo:
+            return memo[(index, diff)]
+
+        if index == len(rods) - 1:
+            if diff == 0 or diff == rods[index]:
+                return 0
+            elif diff == -rods[index]:
+                return rods[index]
+            else:
+                return float('-inf')
+
+        take = self.helper(rods, index+1, diff + rods[index], memo) + rods[index]
+        give = self.helper(rods, index+1, diff - rods[index], memo)
+        skip = self.helper(rods, index+1, diff, memo)
+
+        memo[(index, diff)] = max([take, give, skip])
+        return memo[(index, diff)]
+
+    def tallestBillboard(self, rods: List[int]) -> int:
+        memo = {}
+        return self.helper(rods, 0, 0, memo)
